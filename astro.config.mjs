cfa24814beca0745c96fbbb487853c9251cd1477
Sebastien <ots.sebastien@gmail.com>
2025-07-16 22:33:49 +0200

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://ots-s.github.io',
+  site: 'https://ots-s.github.io/ots-s.github.io',
   base: '/ots-s.github.io/',
 });
